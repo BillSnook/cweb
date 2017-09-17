616abf6c96453074e852a391ae32a973c443d4e1
Bill Snook <bluegoose1@mac.com>
2017-09-17 14:33:31 -0700

    Listen or sender implemented wit defaults

---
+++ client.h
@@ -0,0 +1,13 @@
+//
+//  client.h
+//  cweb
+//
+//  Created by William Snook on 9/17/17.
+//  Copyright © 2017 billsnook. All rights reserved.
+//
+
+#ifndef client_h
+#define client_h
+
+
+#endif /* client_h */
---
+++ server.h
@@ -0,0 +1,13 @@
+//
+//  server.h
+//  cweb
+//
+//  Created by William Snook on 9/17/17.
+//  Copyright © 2017 billsnook. All rights reserved.
+//
+
+#ifndef server_h
+#define server_h
+
+
+#endif /* server_h */
