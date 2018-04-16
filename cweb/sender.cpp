360bbb9a7eb592b223e58c0bb28b9e98af47eaf8
Bill Snook <bluegoose1@mac.com>
2018-04-15 17:07:38 -0700

    Improved sender, debug info

--- cweb/sender.cpp
+++ cweb/sender.cpp
@@ -28,16 +28,21 @@ void Sender::setupSender( char *hostName, int rcvPortNo) {
 		fprintf(stderr,"\nERROR, no such host: %s\n", hostName);
 		exit(0);
 	}
-	fprintf(stderr,"\nGot host: %s, addr: %s\n", server->h_name, server->h_addr);
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+//	char *haddress = (char *)&serv_addr;
+//	fprintf(stderr,"\nGot host: %s, addr: %01X%01X\n", server->h_name, (haddress[0] & 0x0FF), (haddress[1] & 0x0FF));
+	bzero( (char *)&serv_addr, sizeof(serv_addr) );
 	serv_addr.sin_family = AF_INET;
-	int haddr = 0xC0A8011E;
-	bcopy((char *)&haddr, //(char *)server->h_addr, 0xC0A8011E
+//	int haddr = 0xC0A8011E;
+	bcopy((char *)server->h_addr,	// 0xC0A8011E
 		  (char *)&serv_addr.sin_addr.s_addr,
 		  server->h_length);
 	serv_addr.sin_port = htons(portno);
-	if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
-		fprintf(stderr, "\nERROR connecting\n");
+	char *haddress = (char *)&serv_addr;
+	fprintf(stderr,"\nGot addr: %02X %02X %02X%02X %02X%02X%02X%02X\n", haddress[0] & 0xFF, haddress[1] & 0xFF, haddress[2] & 0xFF, haddress[3] & 0xFF, haddress[4] & 0xFF, haddress[5] & 0xFF, haddress[6] & 0xFF, haddress[7] & 0xFF);
+	fprintf(stderr, "\nSetup to connect to socket %04X on %s\n\n", portno, inet_ntoa(serv_addr.sin_addr));
+	int connectResult = connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+	if (connectResult < 0) {
+		fprintf(stderr, "\nERROR connecting: %d\n", connectResult);
 		exit( 0 );
 	}
 	fprintf(stderr, "Please enter the message: ");
