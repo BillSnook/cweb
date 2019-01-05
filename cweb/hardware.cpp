9563529c305539d44a853b44ef1b2d4330b893ba
Bill Snook <bluegoose1@mac.com>
2019-01-05 12:53:17 -0800

    Test

--- cweb/hardware.cpp
+++ cweb/hardware.cpp
@@ -631,7 +631,7 @@ int Ard::testRead() {
 void Ard::testWrite(int data) {
 	
 //	putI2CReg(data);
-	ard_i2c->i2cWrite( 0x43, data );
+	ard_i2c->i2cWrite( 0x43, 0x45 );
 	syslog(LOG_NOTICE, "Wrote 0x%X to I2C device", data & 0x0FF);
 
 }
