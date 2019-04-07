cabd95bee351086b1981fb57ebba9b0bcd6c5c38
Bill Snook <bluegoose1@mac.com>
2019-04-07 14:02:33 -0700

    Update

--- cweb/hardware.cpp
+++ cweb/hardware.cpp
@@ -572,7 +572,7 @@ unsigned int Hardware::ping( unsigned int angle ) {
 	
 //	syslog(LOG_NOTICE, "In ping" );
 	manager.setRange( angle );
-	usleep( 100000 );		// Allow time for servo to move and pulse to propagate and return
+	usleep( 400000 );		// Allow time for servo to move and pulse to propagate and return
 	unsigned int range = (unsigned int)manager.getRange();
 	unsigned int cm = range/29/2;	// 	inches = range/74/2; mm = (range*10)/29/2
 	return cm;
