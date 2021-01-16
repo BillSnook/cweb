99d04a42d8f625b5dae5c7cd716953ace09ee6d2
Bill <billsnook@me.com>
2021-01-16 09:15:56 -0800

    Dbg

--- cweb/commands.cpp
+++ cweb/commands.cpp
@@ -88,9 +88,9 @@ void Commander::serviceCommand( char *command, int sockOrAddr ) {	// Main comman
 		token4 = atoi( nextToken[4] );
 	}
 	char commandType = command[0];	// Get command
-//	for ( int y = 0; y < tokenCount; y++ ) {        // Display all token values
-//		syslog(LOG_NOTICE, "Token %d: %s", y, nextToken[y] );
-//	}
+	for ( int y = 0; y < tokenCount; y++ ) {        // Display all token values
+		syslog(LOG_NOTICE, "Token %d: %s", y, nextToken[y] );
+	}
 
     // Prepare for return msg
 	char msg[ 1024 ]; // Reply back to sender, if non-empty at end of routine
