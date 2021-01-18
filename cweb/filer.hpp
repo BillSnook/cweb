4571a3cb81caeb0fb34588e34a96ea7f489a04f2
Bill <billsnook@me.com>
2021-01-18 09:50:42 -0800

    New files

---
+++ cweb/filer.hpp
@@ -0,0 +1,28 @@
+//
+//  filer.hpp
+//  cweb
+//
+//  Created by Bill Snook on 1/18/21.
+//  Copyright © 2021 billsnook. All rights reserved.
+//
+
+#ifndef filer_hpp
+#define filer_hpp
+
+#include "speed.hpp"
+
+
+class Filer {
+
+    char fileName[64];
+    char *speedFileName = fileName;
+
+public:
+    
+    void setFile( int whichFile );
+    void saveData( speed_array *forward, speed_array *reverse );
+    bool readData( speed_array *forward, speed_array *reverse );
+};
+
+
+#endif /* filer_hpp */
