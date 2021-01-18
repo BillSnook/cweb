38a1d55aa2a577bac9ac3d65e75c5aea19a9a375
Bill <billsnook@me.com>
2021-01-18 14:00:26 -0800

    Fix file problem

--- cweb/filer.cpp
+++ cweb/filer.cpp
@@ -56,7 +56,7 @@ void Filer::getHostName() {
 
 void Filer::setFile( int whichFile ) {
     
-    sprintf( fileName, "%s/%s/%s", SPEED_FILE_PATH, hostName, SPEED_FILE_NAME );
+    sprintf( fileName, "%s/%s", SPEED_FILE_PATH, SPEED_FILE_NAME );
 //    syslog(LOG_NOTICE, "Found speed file path: %s", fileName );
 }
 
