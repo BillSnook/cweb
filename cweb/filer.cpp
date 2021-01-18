9ba4c66ea76e63b20b0c0bcdfa37566103658a03
Bill <billsnook@me.com>
2021-01-18 10:17:55 -0800

    Dbg name

--- cweb/filer.cpp
+++ cweb/filer.cpp
@@ -57,7 +57,7 @@ char *Filer::getHostName() {
 void Filer::setFile( int whichFile ) {
     
     int sizeOfPath = sizeof( SPEED_FILE_PATH );
-    sprintf( fileName, "%s/%s/%s", SPEED_FILE_PATH, hostName, SPEED_NAME );
+    sprintf( fileName, "%s/%s/%s", SPEED_FILE_PATH, hostDirectoryName, SPEED_NAME );
     syslog(LOG_NOTICE, "Found speed file path: %s", fileName );
 
 //    speedFileName = fileName;
