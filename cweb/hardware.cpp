d292428bd63f2a25150487e961ccea923cf86067
Bill <billsnook@me.com>
2021-01-23 13:51:50 -0800

    Dbg

--- cweb/hardware.cpp
+++ cweb/hardware.cpp
@@ -656,7 +656,7 @@ void Hardware::scanPing( int sockOrAddr ) {
                 if ( !scanLoop ) {
                     break;
                 }
-                bzero( buffer, 256 );
+//                bzero( buffer, 256 );
                 siteMap.returnEntry( buffer, angle, distance );
                 listener.writeBack( buffer, sockOrAddr );
 //				syslog(LOG_NOTICE, "scanPing angle: %d, distance: %u cm", angle, distance );
@@ -669,7 +669,7 @@ void Hardware::scanPing( int sockOrAddr ) {
                 if ( !scanLoop ) {
                     break;
                 }
-                bzero( buffer, 256 );
+//                bzero( buffer, 256 );
                 siteMap.returnEntry( buffer, angle, distance );
                 listener.writeBack( buffer, sockOrAddr );
 //				syslog(LOG_NOTICE, "scanPing angle: %d, distance: %u cm", angle, distance );
