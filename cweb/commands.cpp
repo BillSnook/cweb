3308acef4c2656e1c3ea357a5a5a6879f3de7849
Bill <billsnook@me.com>
2021-01-25 12:19:59 -0800

    Retest kill switch

--- cweb/commands.cpp
+++ cweb/commands.cpp
@@ -263,7 +263,7 @@ void Commander::serviceCommand( char *command, int sockOrAddr ) {	// Main comman
         }
         case 'Q':
 //        case 'q':
-            system( "shutdown now" );
+            system( "sudo shutdown now" );
             break;
             
 		case 'R':
