ccbde37b78e9e4ee51c3ae633bbd441b8da56b73
Bill <billsnook@me.com>
2023-08-26 11:46:07 -0700

    Get more data

--- cweb/threader.cpp
+++ cweb/threader.cpp
@@ -208,7 +208,7 @@ void Threader::runNextThread( void *tcPointer ) {
     
     ThreadControl nextThreadControl = *((ThreadControl *)tcPointer);
     threadCount += 1;
-    syslog(LOG_NOTICE, "Run thread with %s, threads: %d", nextThreadControl.description(), threadCount );
+    syslog(LOG_NOTICE, "Run thread with %s, threads: %df, command: %s", nextThreadControl.description(), threadCount, nextThreadControl.nextCommand );
 	switch ( nextThreadControl.nextThreadType ) {
 //		case managerThread:         // Singleton, started first, manages I2C communication
 //			manager.monitor();
